6a19250158eab88a7c4354bfa3dd944904a7a23c
cocomongg <pjm4142@naver.com>
2024-01-28 20:59:55 +0900

    initial commit

---
+++ src/test/java/com/flab/openmarket/OpenMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flab.openmarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
